8df3d7a9f4a6d6366000bd91ba28ceba345f75d0
Varun K V <varunvishwanath1@gmail.com>
2024-06-13 19:00:33 +0530

    Update index.js
    Added DELETE for CROS policy header and "*" for granting all CROS policies.

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.use(
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  // Use this granting all persmissions ("Access-Control-Allow-Methods", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
